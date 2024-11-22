bb81d5a71bc5d592bc0634a97ea67a8d23ea8dec
migben <itsmigben@gmail.com>
2024-11-22 10:27:39 -0500

    add the player to the screen and added movement

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -11,15 +12,34 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    updatable.add(player)
+    drawable.add(player)
 
     while True:
-          for event in pygame.event.get():
+
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-          screen.fill((0,0,0))
 
-          pygame.display.flip()
+        # update all objects in the group
+        for sprite in updatable:
+            sprite.update(dt)
+
+        screen.fill((0,0,0))
+
+        # Draw me all the objects in the drawable group
+        for sprite in drawable:
+            sprite.draw(screen)
+
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
         main()
